759b3bab0a8291f598371ec10c1fa3a7f26da508
WataruOno39 <wataru39ono@gmail.com>
2019-12-15 04:05:22 +0900

    スタイル修正

---
+++ index.js
@@ -0,0 +1,51 @@
+function isScrollReached(percentage) {
+  const contentHeight = $(document).height();
+  const scrollPosition = $(window).height() + $(window).scrollTop();
+  return percentage >= ((contentHeight - scrollPosition) / contentHeight);
+}
+
+function isMobile() {
+  const documentWidth = $(document).width();
+  return documentWidth < 768;
+}
+
+
+window.addEventListener('scroll', () => {
+  if(isScrollReached(0.8)) {
+    $('.js-header').addClass('is-scrolled');
+  } else {
+    $('.js-header').removeClass('is-scrolled');
+  }
+});
+
+function hasIsShow() {
+  if ($(event.target).next('.js-toggleContent').hasClass('is-show')) {
+    return true;
+  }
+}
+
+$(() => {
+  $('.js-toggle').on('click', () => {
+    if(hasIsShow()){  
+      $(event.target).next('.js-toggleContent').removeClass('is-show');
+    } else {
+      $(event.target).next('.js-toggleContent').addClass('is-show');
+    }
+  });
+  
+  const movie = isMobile() ? document.getElementById('js-movie-sp') : document.getElementById('js-movie-pc');
+  
+  $('.js-moviePlay').on('click', () => {
+    movie.play();
+    $('.js-moviePlay').addClass('is-clicked');
+  }); 
+  
+  
+  movie.addEventListener('play', () => {
+     movie.addEventListener('mouseover',() => {
+      movie.setAttribute("controls", "controls");
+    }).removeEventListener('mouseover', () => {
+       movie.removeAttribute("controls");
+     });
+  });
+});
