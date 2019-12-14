7fda878009e4f1f218f342d0dedf22f39daca261
WataruOno39 <wataru39ono@gmail.com>
2019-12-15 07:14:13 +0900

    動作テスト

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ $(() => {
   const movie = isMobile() ? document.getElementById('js-movie-sp') : document.getElementById('js-movie-pc');
   
   $('.js-moviePlay').on('click', () => {
-    console.log(movie);
+    $('#js-movie-sp')[0].play();
     movie.play();
     $('.js-moviePlay').addClass('is-clicked');
   }); 
